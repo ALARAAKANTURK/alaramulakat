a8c549ca03d46654984b79e6ae52c064436063d7
ALARAAKANTURK <alarakanturk@gmail.com>
2024-10-22 15:57:58 +0300

    Update XoX.jsx
    create html structure,  span tag purpose is applying style

--- src/Components/XoX/XoX.jsx
+++ src/Components/XoX/XoX.jsx
@@ -4,6 +4,10 @@ import circle_icon from '../Assets/o.png'
 import cross_icon from '../Assets/x.png'
 export const XoX = () => {
   return (
-    <div>XoX</div>
+    <div className='container' >
+    <h1 className="tittle">XoX Game In <span>React</span></h1>
+
+
+    </div>
   )
 }
