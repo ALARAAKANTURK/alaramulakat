544fd42e05ba20eee2c09af7458867aabd591360
ALARAAKANTURK <alarakanturk@gmail.com>
2024-10-22 15:51:47 +0300

    Update XoX.jsx
    import icon

--- src/Components/XoX/XoX.jsx
+++ src/Components/XoX/XoX.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './XoX.css'
+import circle_icon from '../Assets/o.png'
+import cross_icon from '../Assets/x.png'
 export const XoX = () => {
   return (
     <div>XoX</div>
