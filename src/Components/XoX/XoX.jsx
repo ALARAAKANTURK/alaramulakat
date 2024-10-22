31a5a40124eb1c9b4281fa49a59e7c0e54341973
ALARAAKANTURK <alarakanturk@gmail.com>
2024-10-22 16:45:54 +0300

    Update XoX.jsx
    create rows

--- src/Components/XoX/XoX.jsx
+++ src/Components/XoX/XoX.jsx
@@ -7,7 +7,21 @@ export const XoX = () => {
     <div className='container' >
     <h1 className="tittle">XoX Game In <span>React</span></h1>
     <div className="board">
-
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
     </div>
     <button className="reset">Reset</button>
 
