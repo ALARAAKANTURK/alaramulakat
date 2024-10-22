1fb0a0f5eacd3a4ce93b236b374395fdf6137222
ALARAAKANTURK <alarakanturk@gmail.com>
2024-10-22 20:49:02 +0300

    Update XoX.jsx
    check winner after every check

--- src/Components/XoX/XoX.jsx
+++ src/Components/XoX/XoX.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import { useRef } from 'react';
 import './XoX.css'
 import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 
 let data =["","","","","","","","",""];
 
@@ -9,6 +11,7 @@ let data =["","","","","","","","",""];
 export const XoX = () => {
     let[count,setCount] = useState(0);
     let[lock,setLock]=useState(false);
+    let titleRef = useRef(null);
 
    const toggle = (e,num) => {
       if(lock)
@@ -31,19 +34,67 @@ export const XoX = () => {
             data[num]="o";
             setCount(++count);
           }
+          checkwin();
 
    } 
    const checkwin = () => {
     if(data[0]==data[1]&&data[1]==data[2]&&data[2]!=="")
     {
-        
+        won(data);
+          
     }
+    else if(data[3]==data[4]&&data[4]==data[5]&&data[5]!=="")
+    {
+        won(data);
+    }
+    else if(data[6]==data[7]&&data[7]==data[8]&&data[8]!=="")
+    {
+        won(data);
+    }
+    else if(data[0]==data[3]&&data[3]==data[6]&&data[6]!=="")
+    {
+        won(data);
+    }
+    else if(data[1]==data[4]&&data[4]==data[7]&&data[7]!=="")
+    {
+        won(data);
+    }
+    else if(data[2]==data[5]&&data[5]==data[8]&&data[8]!=="")
+    {
+        won(data);
+    }
+    else if(data[0]==data[4]&&data[4]==data[8]&&data[8]!=="")
+    {
+        won(data);
+    }
+    else if(data[0]==data[1]&&data[1]==data[2]&&data[2]!=="")
+    {
+        won(data);
+    }
+    else if(data[2]==data[4]&&data[4]==data[6]&&data[6]!=="")
+    {
+        won(data);
+    }
+   }
+   const won = (winner) =>{
+    setLock(true);
+    if(winner="x")
+    {
+        titleRef.current.innerHTML=`Congralutions: <img src='${cross_icon}' alt='cross'>`;
+
+    }
+    else
+    {
+        
+        titleRef.current.innerHTML=`Congralutions: <img src='${circle_icon}' alt='circle'>`;
+    }
+
    }
 
 
   return (
     <div className='container' >
-    <h1 className="tittle">XoX Game In <span>React</span></h1>
+    <h1 className="title"ref={titleRef}>XoX Game In <span>React</span></h1>
     <div className="board">
         <div className="row1">
             <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
